8a448dd4f2c0afc6448452cf036ad4a638a941b0
kevin <chaguakevin@gmail.com>
2018-08-20 16:59:34 -0500

    Home

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import { Media } from 'reactstrap';
+import Lentes from '../../images/lentes.jpg';
+import Android from '../../images/androide.png';
+import Apple from '../../images/apple.png';
+import './Home.css';
+
+import {
+    Container,
+    Row,
+    Col
+} from 'reactstrap';
 
 class Home extends React.Component{
     render(){
         return(
-            <h1>
-                My Home
-            </h1>
+            <Container>
+                <Row>
+                    <div className="root">
+                        <div className="Home">
+                            <img src={Lentes}/>
+                        </div>
+                        <div className="Android">
+                            <img src={Android}/>
+                        </div>
+                        <div className="Apple">
+                            <img src={Apple}/>
+                        </div>
+                    </div>
+                </Row>
+            </Container>
         )
     }
 }
